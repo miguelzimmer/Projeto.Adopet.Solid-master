ccee9ae8d455287864f4cdcc3c058c2efbc8a8d1
MIGUEL ZIMMERMANN <miguelzimmer88@gmail.com>
2024-02-15 08:26:27 -0300

    Initial commit

---
+++ src/Alura.Adopet.Console/Servicos/Arquivos/LeitorDeArquivoJson.cs
@@ -0,0 +1,23 @@
+﻿
+using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos.Abstracoes;
+using System.Text.Json;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class LeitorDeArquivoJson: ILeitorDeArquivos
+    {
+        private string caminhoArquivo;
+
+        public LeitorDeArquivoJson(string caminhoArquivo)
+        {
+            this.caminhoArquivo = caminhoArquivo;
+        }
+
+        public IEnumerable<Pet> RealizaLeitura()
+        {
+            using var stream = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+            return JsonSerializer.Deserialize<IEnumerable<Pet>>(stream)??Enumerable.Empty<Pet>();
+        }
+    }
+}
---
+++ tests/Alura.Adopet.Testes/Servicos/LeitorDeArquivosJsonTest.cs
@@ -0,0 +1,59 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos.Arquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Testes.Servicos
+{
+    public class LeitorDeArquivosJsonTest : IDisposable
+    {
+        private string caminhoArquivo;
+        public LeitorDeArquivosJsonTest()
+        {
+            //Setup
+            string conteudo = @"
+            [
+              {
+                ""Id"": ""68286fbf-f6f4-4924-adab-0637511813e0"",
+                ""Nome"": ""Mancha"",
+                ""Tipo"": 1
+              },
+              {
+                ""Id"": ""68286fbf-f6f4-4924-adab-0637511672e0"",
+                ""Nome"": ""Alvo"",
+                ""Tipo"": 1
+              },
+              {
+                ""Id"": ""68286fbf-f6f4-1234-adab-0637511672e0"",
+                ""Nome"": ""Pinta"",
+                ""Tipo"": 1
+              }
+            ]
+        ";
+
+            string nomeRandomico = $"{Guid.NewGuid()}.json";
+
+            File.WriteAllText(nomeRandomico, conteudo);
+            caminhoArquivo = Path.GetFullPath(nomeRandomico);
+        }
+
+        [Fact]
+        public void QuandoArquivoExistenteDeveRetornarUmaListaDePets()
+        {
+            //Arrange            
+            //Act
+            var listaDePets = new LeitorDeArquivoJson(caminhoArquivo).RealizaLeitura()!;
+            //Assert
+            Assert.NotNull(listaDePets);
+            Assert.IsType<List<Pet>?>(listaDePets);
+        }
+        public void Dispose()
+        {
+            //ClearDown
+            File.Delete(caminhoArquivo);
+        }
+    }
+}
